add9a5c3f43a5ee80c247d368cfe4ef1c71dda83
Lukelectro <elektronica@deds.nl>
2020-11-13 22:41:39 +0100

    add files from previous juggle ball project. Still have to modify these, but want them in git so even if a cubeMX code regenerate deletest them they are easy to restore

---
+++ Core/Inc/config.h
@@ -0,0 +1,23 @@
+#ifndef __CONFIG
+#define __CONFIG
+
+/* TODO: modify / complete for smallrgbjuggle */
+
+#define LEN_COLOR 33
+const int colorset_percentage[LEN_COLOR] = //r,g,b
+{
+ 100,0,0,    //red
+ 100,30,100, //white-ish purple
+ 100,0,30,   //magenta/pink purple
+ 100,60,0,   //yellow
+ 0,0,100,     //blue
+ 0,100,0,    //green
+ 30,0,100,		// untested... Blue Purple-ish?
+ 100,100,100, // white 
+ 0,100,100,    //bluegreen
+ 100,0,100,   // purple
+ 0,100,60	// untested... greenish Blueish?
+ }; 
+
+#endif
+
